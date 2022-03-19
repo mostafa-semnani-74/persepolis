b895ea966e7eb35dfd94621e46064a966e11faed
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2022-03-19 08:46:37 +0330

    initial

---
+++ src/main/java/ir/mosi/persepolis/PersepolisApplication.java
@@ -0,0 +1,13 @@
+package ir.mosi.persepolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersepolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersepolisApplication.class, args);
+	}
+
+}
